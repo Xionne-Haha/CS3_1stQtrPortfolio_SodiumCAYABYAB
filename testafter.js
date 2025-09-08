d9169b9cc987a6b48eb7a830d97f202497fc0f5b
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2025-09-08 14:22:14 +0800

    Fifth commit

--- testafter.js
+++ testafter.js
@@ -10,23 +10,23 @@ let age;
 function get_line(age){
    
      if ( age == 13){
-        console.log("You can watch movies with ratings: G and PG (with a parent).");
+        console.log("You can watch movies with ratings: G and PG (with a parent).")
         
     }
     else if(13 <= age < 16){
-        console.log("You can watch movies with ratings: G, PG, and R-13.");
+        console.log("You can watch movies with ratings: G, PG, and R-13.")
         
     }
     else if(16 <= age < 18){
-        console.log("You can watch movies with ratings: G, PG, R-13, and R-16.");
+        console.log("You can watch movies with ratings: G, PG, R-13, and R-16.")
         
     }
     else if(age >= 18){
-        console.log("You can watch movies with ratings: G, PG, R-13, R-16, R-18 .");
+        console.log("You can watch movies with ratings: G, PG, R-13, R-16, R-18 .")
         
     }
     else{
-        console.log("You can watch movies with ratings: G.");
+        console.log("You can watch movies with ratings: G.")
         
     }
 }
