384a42b3888d991b0eb5efc4709dc0f48be0f3c1
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2025-09-08 14:14:48 +0800

    Fifth Commit

---
+++ loops.js
@@ -0,0 +1,23 @@
+for (let i = 1; i<=12; i++){
+    console.log(i)
+}
+
+// this is the start: let i = 1;
+// condition: i<=12;
+// update: i++
+
+i=1;
+while(i<=12){
+    console.log(i)
+    i++
+}
+
+// do-while loop is a loop when you wnat something to happen at least once
+
+i=1;
+do{
+    console.log(i);
+    i++
+} while(i<=3);
+
+console.log("hello")
