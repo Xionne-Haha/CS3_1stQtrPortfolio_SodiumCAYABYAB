8bffc73b4a301182811596a6b28f7572d3e3870b
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2025-09-08 14:18:04 +0800

    Fifth Commit

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+console.log("For the for loop")
 for (let i = 1; i<=12; i++){
     console.log(i)
 }
@@ -6,6 +7,10 @@ for (let i = 1; i<=12; i++){
 // condition: i<=12;
 // update: i++
 
+console.log("  ")
+console.log("  ")
+console.log("  ")
+console.log("For the while loop")
 i=1;
 while(i<=12){
     console.log(i)
@@ -14,10 +19,22 @@ while(i<=12){
 
 // do-while loop is a loop when you wnat something to happen at least once
 
+console.log("  ")
+console.log("  ")
+console.log("  ")
+console.log("For the do-while loop")
 i=1;
 do{
     console.log(i);
     i++
 } while(i<=3);
 
-console.log("hello")
+
+console.log("  ")
+console.log("  ")
+console.log("  ")
+console.log("For the even number for loop")
+
+for (let i = 2; i<=6; i = i +2){
+    console.log(i)
+}
